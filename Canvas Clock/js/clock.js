e15d5dddf63a5b3749df2563fabf58a712e17653
rahnis <rahulnishant13@gmail.com>
2015-12-21 18:26:56 +0530

    Submit

---
+++ Canvas Clock/js/clock.js
@@ -0,0 +1,65 @@
+	var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    var myMusic = document.getElementById("myAudio");
+    
+    ctx.strokeStyle = '#99FF00';
+    ctx.lineWidth = 17;
+    ctx.lineCap = 'round';
+    ctx.shadowBlur = 15;
+    ctx.shadowColor = '#99FF00';
+
+
+function degToRad(degree){
+	var factor = Math.PI / 180;
+	return degree*factor;
+}
+
+function playMusic(){
+    if( (time > 1 || time < 24) && (minutes == 20) ){
+    	myMusic.play();
+    }
+    
+}
+
+function renderTime(){
+	
+	var now = new Date();
+	var today = now.toDateString();
+	var time = now.toLocaleTimeString();
+	var hours = now.getHours();
+	var minutes = now.getMinutes();
+	var sec = now.getSeconds();
+	var milliseconds = now.getMilliseconds();
+	var newSec = sec + (milliseconds / 1000);
+	
+	// backgrond
+	ctx.fillRect(0,0,500,500);
+	ctx.fillStyle = 'BLACK';
+	
+	
+	// hours
+
+    ctx.beginPath();
+    ctx.arc(250, 250, 200, degToRad(270), degToRad((hours*15) - 90));
+    ctx.stroke();
+	
+	// minutes
+    ctx.beginPath();
+    ctx.arc(250, 250, 170, degToRad(270), degToRad((minutes*6) - 90));
+    ctx.stroke();
+	
+	//sec
+    ctx.beginPath();
+    ctx.arc(250, 250, 140, degToRad(270), degToRad((newSec*6) - 90));
+    ctx.stroke();
+    
+	//date
+    ctx.font = '25px Arial Bold';
+    ctx.fillText(today, 175, 250);
+    
+    //time
+    ctx.font = '25px Arial';
+    ctx.fillText(time, 175, 200);
+}
+setInterval(renderTime, 40);
+playMusic();
